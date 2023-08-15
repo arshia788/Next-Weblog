bb5f52b3a19606bfb58266e8361c6d1f7dae7be9
Amirali Amanipour <arshia.ala80@gmail.com>
2023-08-16 02:20:31 +0330

    initial folder

---
+++ components/layout/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Layout( {children} ) {
+  return (
+    <div>
+        <header>
+
+        </header>
+
+        <div>
+            {children}
+        </div>
+
+        <footer>
+            
+        </footer>
+    </div>
+  )
+}
+
+export default Layout
